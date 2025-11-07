5f7e2b6d32bdbddd01f2f0e7ee95c3ff30d5dd4b
Pedro Augusto <79765850+PedroASB@users.noreply.github.com>
2025-11-07 16:35:24 -0300

    feat: implement 'calculator'

--- src/main.js
+++ src/main.js
@@ -9,3 +9,13 @@ export function reverseString(string) {
   }
   return reversedString;
 }
+
+export const calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+    if (b === 0) throw Error('Math Error: division by zero');
+    return a / b;
+  },
+};
