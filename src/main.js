92afb1d91a0b19a0751224f4cc502e9cb019e939
Pedro Augusto <79765850+PedroASB@users.noreply.github.com>
2025-11-07 18:49:58 -0300

    feat: implement 'caesarCipher'

--- src/main.js
+++ src/main.js
@@ -19,3 +19,34 @@ export const calculator = {
     return a / b;
   },
 };
+
+// checks if character is in alphabet and is lowercase
+function isLowercaseAlpha(character) {
+  return character.charCodeAt(0) >= 97 && character.charCodeAt(0) <= 122;
+}
+
+// checks if character is in alphabet and is uppercase
+function isUppercaseAlpha(character) {
+  return character.charCodeAt(0) >= 65 && character.charCodeAt(0) <= 90;
+}
+
+export function caesarCipher(string, shift) {
+  let cipherString = '';
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+    if (isLowercaseAlpha(char)) {
+      char = char.charCodeAt(0) + shift;
+      if (char > 122) char = (char % 122) + 96;
+      cipherString += String.fromCharCode(char);
+    } else if (isUppercaseAlpha(char)) {
+      char = char.charCodeAt(0) + shift;
+      if (char > 90) char = (char % 90) + 64;
+      cipherString += String.fromCharCode(char);
+    } else {
+      cipherString += char;
+    }
+  }
+
+  return cipherString;
+}
