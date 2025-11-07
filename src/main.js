7c9c821a2da48e50e0df3b652288a5cc39310499
Pedro Augusto <79765850+PedroASB@users.noreply.github.com>
2025-11-07 16:21:42 -0300

    feat: implement 'reverseString'

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 export function capitalize(string) {
   return string.replace(string[0], string[0].toUpperCase());
 }
+
+export function reverseString(string) {
+  let reversedString = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  return reversedString;
+}
